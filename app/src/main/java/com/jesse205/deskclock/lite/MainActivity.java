d6694630f95837a10290c7b41e8ae1618d88c83b
DESKTOP-9PJHLMI\USER <2055675594@qq.com>
2022-11-03 01:25:01 +0800

    添加可自定义大小的活动，修复英语语言下无法使用的问题

--- app/src/main/java/com/jesse205/deskclock/lite/MainActivity.java
+++ app/src/main/java/com/jesse205/deskclock/lite/MainActivity.java
@@ -28,6 +28,7 @@ public class MainActivity extends Activity {
                     WindowManager.LayoutParams.FLAG_FULLSCREEN);
         }
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
         TextView timeView = findViewById(R.id.time);
         TextView dateView = findViewById(R.id.date);
         String time_template = getString(R.string.time_template);
@@ -44,13 +45,15 @@ public class MainActivity extends Activity {
                     String time = timeFormat.format(new Date());
                     timeView.setText(time);
 
-                    String date = String.format((String) date_template, getWeekDay(calendar.get(Calendar.DAY_OF_WEEK)));
                     @SuppressLint("SimpleDateFormat")
-                    SimpleDateFormat dateFormat = new SimpleDateFormat(date);
-                    String date2 = dateFormat.format(new Date());
-                    dateView.setText(date2);
+                    SimpleDateFormat dateFormat = new SimpleDateFormat(date_template);
+                    String baseDate = dateFormat.format(new Date());
+                    String date = baseDate + " " + getWeekDay(calendar.get(Calendar.DAY_OF_WEEK));
+                    dateView.setText(date);
 
-                    handler.postDelayed(this, 100);
+                    if (activitStarted) {
+                        handler.postDelayed(this, 100);
+                    }
                 }
             }
         };
@@ -71,17 +74,50 @@ public class MainActivity extends Activity {
 
     }
 
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            //if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            View decorView = getWindow().getDecorView();
+
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+
+                // Set the content to appear under the system bars so that the
+                // content doesn't resize when the system bars hide and show.
+                int visibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        // Hide the nav bar and status bar
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN;
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                    visibility |= View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+                }
+                decorView.setSystemUiVisibility(visibility);
+            }
+        }
+
+
+    }
+
     @TargetApi(Build.VERSION_CODES.KITKAT)
     @Override
     protected void onResume() {
         super.onResume();
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-        int flags = View.SYSTEM_UI_FLAG_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_IMMERSIVE
-                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION;
-            getWindow().getDecorView().setSystemUiVisibility(flags);
+            View decorView = getWindow().getDecorView();
+            decorView.setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_IMMERSIVE
+                            // Set the content to appear under the system bars so that the
+                            // content doesn't resize when the system bars hide and show.
+                            | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            // Hide the nav bar and status bar
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN);
         }
     }
 
