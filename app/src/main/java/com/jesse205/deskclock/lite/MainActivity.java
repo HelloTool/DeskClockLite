ad483264981122d3bda231b44839994cfe657574
DESKTOP-9PJHLMI\USER <2055675594@qq.com>
2022-11-03 04:15:06 +0800

    新增亮色主题，允许安装到SD卡，优化代码

--- app/src/main/java/com/jesse205/deskclock/lite/MainActivity.java
+++ app/src/main/java/com/jesse205/deskclock/lite/MainActivity.java
@@ -3,26 +3,52 @@ package com.jesse205.deskclock.lite;
 import android.annotation.SuppressLint;
 import android.annotation.TargetApi;
 import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.preference.PreferenceManager;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
-public class MainActivity extends Activity {
-    boolean activitStarted = false;
-    Handler handler;
-    Runnable runnable;
+public class MainActivity extends Activity implements View.OnTouchListener, GestureDetector.OnGestureListener {
+    private boolean lightTheme = false;
+    private boolean activityStarted = false;
+    private Handler handler;
+    private Runnable runnable;
+    private SharedPreferences sharedPreferences;
+    private GestureDetector mGestureDetector;
 
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        lightTheme = sharedPreferences.getBoolean("light_theme", false);
+        if (lightTheme)
+            setTheme(R.style.Theme_DeskClockLite_Light);
+        else
+            setTheme(R.style.Theme_DeskClockLite);
+
         setContentView(R.layout.activity_main);
+        mGestureDetector = new GestureDetector(this);
+        mGestureDetector.setIsLongpressEnabled(true);
+        FrameLayout mainLayout = findViewById(R.id.mainLayout);
+        mainLayout.setFocusable(true);
+        mainLayout.setClickable(true);
+        mainLayout.setLongClickable(true);
+        mainLayout.setOnTouchListener(this);
+
         if (Build.VERSION.SDK_INT < 16) {
             getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                     WindowManager.LayoutParams.FLAG_FULLSCREEN);
@@ -39,7 +65,7 @@ public class MainActivity extends Activity {
         runnable = new Runnable() {
             @Override
             public void run() {
-                if (activitStarted) {
+                if (activityStarted) {
                     @SuppressLint("SimpleDateFormat")
                     SimpleDateFormat timeFormat = new SimpleDateFormat(time_template);
                     String time = timeFormat.format(new Date());
@@ -51,9 +77,9 @@ public class MainActivity extends Activity {
                     String date = baseDate + " " + getWeekDay(calendar.get(Calendar.DAY_OF_WEEK));
                     dateView.setText(date);
 
-                    if (activitStarted) {
+                    if (activityStarted)
                         handler.postDelayed(this, 100);
-                    }
+
                 }
             }
         };
@@ -63,13 +89,13 @@ public class MainActivity extends Activity {
     @Override
     protected void onStop() {
         super.onStop();
-        activitStarted = false;
+        activityStarted = false;
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        activitStarted = true;
+        activityStarted = true;
         handler.postDelayed(runnable, 0);
 
     }
@@ -81,9 +107,7 @@ public class MainActivity extends Activity {
             //if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
             View decorView = getWindow().getDecorView();
 
-
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
-
                 // Set the content to appear under the system bars so that the
                 // content doesn't resize when the system bars hide and show.
                 int visibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
@@ -128,11 +152,8 @@ public class MainActivity extends Activity {
     }
 
     private String getWeekDay(int dayNumber) {
-        int weekNameId;
+        int weekNameId = R.string.week_1;//case 1 在这里
         switch (dayNumber) {
-            case 1:
-                weekNameId = R.string.week_1;
-                break;
             case 2:
                 weekNameId = R.string.week_2;
                 break;
@@ -151,9 +172,51 @@ public class MainActivity extends Activity {
             case 7:
                 weekNameId = R.string.week_7;
                 break;
-            default:
-                weekNameId = R.string.week_1;
         }
         return getString(weekNameId);
+    }
+
+    @Override
+    public boolean onDown(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+        return false;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent e) {
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putBoolean("light_theme", !lightTheme);
+        editor.apply();
+
+        // 重启活动
+        Intent intent = new Intent(this, this.getClass());
+        startActivity(intent);
+        finish();
+        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+    }
+
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+        return false;
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        return mGestureDetector.onTouchEvent(event);
     }
 }
