ae46c0f52755b6806227d994d768c90aa8142eeb
Jesse205 <2055675594@qq.com>
2024-01-24 20:24:27 +0800

    删除.idea，web添加html-loader

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -35,20 +35,24 @@ const config = {
         // Learn more about plugins from https://webpack.js.org/configuration/plugins/
     ],
     module: {
-        rules: [{
-            test: /\.css$/i,
-            use: [stylesHandler, 'css-loader', 'postcss-loader'],
-        },
-        {
-            test: /\.(ts|tsx)$/i,
-            loader: 'ts-loader',
-            exclude: ['/node_modules/'],
-        },
-
-        {
-            test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-            type: 'asset',
-        },
+        rules: [
+            {
+                test: /\.html$/,
+                loader: 'html-loader'
+            },
+            {
+                test: /\.css$/i,
+                use: [stylesHandler, 'css-loader', 'postcss-loader'],
+            },
+            {
+                test: /\.(ts|tsx)$/i,
+                loader: 'ts-loader',
+                exclude: ['/node_modules/'],
+            },
+            {
+                test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+                type: 'asset',
+            },
 
             // Add your rules for custom modules here
             // Learn more about loaders from https://webpack.js.org/loaders/
